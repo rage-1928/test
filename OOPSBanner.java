f526afeedcdb0fe7f3417651b4aa78441118bfbe
rage-1928 <aarohan192813@gmail.com>
2026-02-19 23:45:22 +0530

    Done

---
+++ OOPSBanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class OOPSBanner
+{
+    public static void main(String args[]) {
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *   *    *   *     ");
+        System.out.println("*     *  *     *   *****    ***** ");
+        System.out.println("*     *  *     *   *            * ");
+        System.out.println(" *****    *****    *        ***** ");
+    }
+}
