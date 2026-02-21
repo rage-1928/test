c3bb71833d21ee0cf8aeaa2665762b60c2810795
rage-1928 <aarohan192813@gmail.com>
2026-02-21 16:11:50 +0530

    UC4 FIXED

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,14 +1,16 @@
 class OOPSBanner {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            if (i == 1 || i == 5)
+            if (i == 1)
                 System.out.println(" *****    *****    *****    ***** ");
             else if (i == 3)
                 System.out.println("*     *  *     *   *****    ***** ");
             else if (i == 2)
                 System.out.println("*     *  *     *   *    *   *     ");
-            else
+            else if(i == 4)
                 System.out.println("*     *  *     *   *            * ");
+            else
+                System.out.println(" *****    *****    *        ***** ");
         }
     }
 }
